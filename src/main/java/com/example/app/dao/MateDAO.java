700509df899715e8e92c787921937c7e38096640
JinHyukKang <jhkang0330@gmail.com>
2023-08-31 21:57:49 +0900

    강진혁 0831 2157 메이트 글작성 시작

--- src/main/java/com/example/app/dao/MateDAO.java
+++ src/main/java/com/example/app/dao/MateDAO.java
@@ -1,5 +1,22 @@
 package com.example.app.dao;
 
+import org.apache.ibatis.session.SqlSession;
+
+import com.example.app.dto.MateDTO;
+import com.mybatis.config.MyBatisConfig;
+
 public class MateDAO {
 
+	public SqlSession sqlSession;
+
+	public MateDAO() {
+		sqlSession = MyBatisConfig.getSqlSessionFactory().openSession(true);
+	}
+	
+	public void write(MateDTO mateDTO) {
+		sqlSession.insert("mate.write",mateDTO);
+	}
+	
+	
+	
 }
--- src/main/java/com/example/app/dto/MateDTO.java
+++ src/main/java/com/example/app/dto/MateDTO.java
@@ -1,5 +1,138 @@
 package com.example.app.dto;
 
-public class MateDTO {
+import java.security.Timestamp;
+import java.util.Date;
 
+public class MateDTO {
+	
+	
+	private int mateNum;
+	private int userNum;
+	private String mateCourtname;
+	private String mateCourtddr;
+	private Date mateDate; 
+	private Timestamp mateStarttime;
+	private Timestamp mateEndtime;
+	private int mateExp;
+	private int mateNtrp;
+	private String mateGametype; 
+	private int mateMcount;
+	private int mateWcount;
+	private String mateContent; 
+	private int commentCnt;
+	private int atstatus;
+	private Date mateWritedate;
+	
+	//게터, 세터
+	public int getMateNum() {
+		return mateNum;
+	}
+	public void setMateNum(int mateNum) {
+		this.mateNum = mateNum;
+	}
+	public int getUserNum() {
+		return userNum;
+	}
+	public void setUserNum(int userNum) {
+		this.userNum = userNum;
+	}
+	public String getMateCourtname() {
+		return mateCourtname;
+	}
+	public void setMateCourtname(String mateCourtname) {
+		this.mateCourtname = mateCourtname;
+	}
+	public String getMateCourtddr() {
+		return mateCourtddr;
+	}
+	public void setMateCourtddr(String mateCourtddr) {
+		this.mateCourtddr = mateCourtddr;
+	}
+	public Date getMateDate() {
+		return mateDate;
+	}
+	public void setMateDate(Date mateDate) {
+		this.mateDate = mateDate;
+	}
+	public Timestamp getMateStarttime() {
+		return mateStarttime;
+	}
+	public void setMateStarttime(Timestamp mateStarttime) {
+		this.mateStarttime = mateStarttime;
+	}
+	public Timestamp getMateEndtime() {
+		return mateEndtime;
+	}
+	public void setMateEndtime(Timestamp mateEndtime) {
+		this.mateEndtime = mateEndtime;
+	}
+	public int getMateExp() {
+		return mateExp;
+	}
+	public void setMateExp(int mateExp) {
+		this.mateExp = mateExp;
+	}
+	public int getMateNtrp() {
+		return mateNtrp;
+	}
+	public void setMateNtrp(int mateNtrp) {
+		this.mateNtrp = mateNtrp;
+	}
+	public String getMateGametype() {
+		return mateGametype;
+	}
+	public void setMateGametype(String mateGametype) {
+		this.mateGametype = mateGametype;
+	}
+	public int getMateMcount() {
+		return mateMcount;
+	}
+	public void setMateMcount(int mateMcount) {
+		this.mateMcount = mateMcount;
+	}
+	public int getMateWcount() {
+		return mateWcount;
+	}
+	public void setMateWcount(int mateWcount) {
+		this.mateWcount = mateWcount;
+	}
+	public String getMateContent() {
+		return mateContent;
+	}
+	public void setMateContent(String mateContent) {
+		this.mateContent = mateContent;
+	}
+	public int getCommentCnt() {
+		return commentCnt;
+	}
+	public void setCommentCnt(int commentCnt) {
+		this.commentCnt = commentCnt;
+	}
+	public int getAtstatus() {
+		return atstatus;
+	}
+	public void setAtstatus(int atstatus) {
+		this.atstatus = atstatus;
+	}
+	public Date getMateWritedate() {
+		return mateWritedate;
+	}
+	public void setMateWritedate(Date mateWritedate) {
+		this.mateWritedate = mateWritedate;
+	}
+	
+	@Override
+	public String toString() {
+		return "MateDTO [mateNum=" + mateNum + ", userNum=" + userNum + ", mateCourtname=" + mateCourtname
+				+ ", mateCourtddr=" + mateCourtddr + ", mateDate=" + mateDate + ", mateStarttime=" + mateStarttime
+				+ ", mateEndtime=" + mateEndtime + ", mateExp=" + mateExp + ", mateNtrp=" + mateNtrp + ", mateGametype="
+				+ mateGametype + ", mateMcount=" + mateMcount + ", mateWcount=" + mateWcount + ", mateContent="
+				+ mateContent + ", commentCnt=" + commentCnt + ", atstatus=" + atstatus + ", mateWritedate="
+				+ mateWritedate + "]";
+	}
+	
+	
+	
+	
+	
 }
