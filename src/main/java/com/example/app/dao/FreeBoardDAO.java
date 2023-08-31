05d831256380db664ceff545714f05b94666d4e8
nye0101 <nayeaeun0101@gmail.com>
2023-08-31 21:07:32 +0900

    나예은 0831 2006 자유게시판(DAO, DTO), 자유게시판 댓글(DAO, DTO), 자유게시판 Mapper 내용 수정

--- src/main/java/com/example/app/dao/FreeBoardDAO.java
+++ src/main/java/com/example/app/dao/FreeBoardDAO.java
@@ -1,5 +1,16 @@
 package com.example.app.dao;
 
+import org.apache.ibatis.session.SqlSession;
+
+import com.mybatis.config.MyBatisConfig;
+
 public class FreeBoardDAO {
+	
+	public SqlSession sqlSession;
+	
+	public FreeBoardDAO() {
+		sqlSession = MyBatisConfig.getSqlSessionFactory().openSession(true);
+	}
+	
 
 }
